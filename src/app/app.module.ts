6f0063be692469f49068f728e78141c50716492d
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2018-07-17 01:13:44 -0300

    Task #16 - criação do sistema de internacionalização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 // Angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 // PrimeNG
@@ -9,10 +10,18 @@ import { SidebarModule } from 'primeng/sidebar';
 
 // Application
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 import { NavbarSidebarComponent } from './navbar-sidebar/navbar-sidebar.component';
-import { LoginComponent } from './login/login.component';
+
+// ngx-translate
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(httpClient : HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -26,10 +35,20 @@ import { LoginComponent } from './login/login.component';
     // Angular
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
 
     // PrimeNG
     ButtonModule,
-    SidebarModule
+    SidebarModule,
+
+    // ngx-translate
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     Title
